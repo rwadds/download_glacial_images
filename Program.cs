b27a7312d43ddd0d00fe3187343925726c430658
Roderic Wadds <rawadds@reddoor.biz>
2024-11-02 13:11:19 -0700

    updated

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
-﻿using static Http;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.Json;
+using static Http;
 
 var filename = @"H:\inpath\nagap_glacier_photos.csv";
 var outpath = @"H:\outpath";
+var checkpath = @"H:\jpg";
+
 var lines = await File.ReadAllLinesAsync(filename);
 var cnt = 0;
 foreach (var line in lines)
 {
     if (cnt == 0) { cnt++; continue; }
     var split = line.Split(',');
+    if (File.Exists(Path.Combine(checkpath, Path.GetFileNameWithoutExtension(split[8]),".jpg"))) continue;    
     var outfile = Path.Combine(outpath, split[8]);
     if (File.Exists(outfile)) continue;
     Console.Write("Downloading..." + split[8]);
     var resp = await Call(HttpVerbs.GET, "https://arcticdata.io/metacat/d1/mn/v2/object/" + split[10]);
     await File.WriteAllBytesAsync(outfile, resp.Response);
+    Console.Write("...Converting..." + split[8]);
+    Converter.ConvertTiffToJpeg(outfile);
+    Console.Write("...Deleting..." + split[8]);
+    if (File.Exists(outfile))
+    {
+        File.Delete(outfile);
+    }
     Console.WriteLine("...Done");
 }
 public static class Http
@@ -79,5 +92,29 @@ public static class Http
         //     Represents a request for information about the communication options available
         //     on the request/response chain identified by the Request-URI.
         OPTIONS = 0x40
+    }
+}
+
+internal static class Converter
+{
+    const string OUTPATH = @"H:\jpg";
+    public static string[] ConvertTiffToJpeg(string fileName)
+    {
+        using var imageFile = Image.FromFile(fileName);
+        var frameDimensions = new FrameDimension(imageFile.FrameDimensionsList[0]);
+        var frameNum = imageFile.GetFrameCount(frameDimensions);         // Gets the number of pages from the tiff image (if multipage) 
+        var jpegPaths = new string[frameNum];
+
+        for (var frame = 0; frame < frameNum; frame++)
+        {
+            // Selects one frame at a time and save as jpeg. 
+            imageFile.SelectActiveFrame(frameDimensions, frame);
+            using var bmp = new Bitmap(imageFile);
+            jpegPaths[frame] = String.Format("{0}\\{1}.jpg", OUTPATH, Path.GetFileNameWithoutExtension(fileName));
+            bmp.Save(jpegPaths[frame], ImageFormat.Jpeg);
+        }
+
+        return jpegPaths;
+
     }
 }
